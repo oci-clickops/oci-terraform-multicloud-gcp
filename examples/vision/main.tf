13e1dfcccf2c924fe94a6ad9b69cd5ae9d40f413
David Gutierrez <david.gutierrezcolodra@gmail.com>
2026-05-17 07:29:51 +0000

    refactor(vision): simplify example to pass-through any-typed config maps

---
+++ examples/vision/main.tf
@@ -0,0 +1,19 @@
+# Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+module "oracle_database_at_gcp" {
+  source = "../.."
+
+  default_project_id          = var.project_id
+  default_location            = var.location
+  default_gcp_oracle_zone     = var.gcp_oracle_zone
+  default_deletion_protection = var.default_deletion_protection
+  default_labels              = var.default_labels
+
+  default_cloud_exadata_maintenance_window = var.default_cloud_exadata_maintenance_window
+
+  gcp_odb_networks_configuration                  = var.gcp_odb_networks_configuration
+  gcp_odb_subnets_configuration                   = var.gcp_odb_subnets_configuration
+  gcp_cloud_exadata_infrastructures_configuration = var.gcp_cloud_exadata_infrastructures_configuration
+  gcp_cloud_vm_clusters_configuration             = var.gcp_cloud_vm_clusters_configuration
+}
---
+++ examples/vision/variables.tf
@@ -0,0 +1,45 @@
+# Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+variable "project_id" { description = "GCP project ID enabled for Oracle Database@Google Cloud." }
+variable "location"   { description = "GCP region for Oracle Database@Google Cloud resources." }
+
+variable "gcp_oracle_zone" {
+  type    = any
+  default = null
+}
+
+variable "default_labels" {
+  type    = any
+  default = null
+}
+
+variable "default_deletion_protection" {
+  type    = any
+  default = false
+}
+
+variable "default_cloud_exadata_maintenance_window" {
+  type    = any
+  default = null
+}
+
+variable "gcp_odb_networks_configuration" {
+  type    = any
+  default = null
+}
+
+variable "gcp_odb_subnets_configuration" {
+  type    = any
+  default = null
+}
+
+variable "gcp_cloud_exadata_infrastructures_configuration" {
+  type    = any
+  default = null
+}
+
+variable "gcp_cloud_vm_clusters_configuration" {
+  type    = any
+  default = null
+}
